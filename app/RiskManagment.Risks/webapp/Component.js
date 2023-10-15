fcce8b940f00c413136a39c0bd1e9511ab7fc24b
Mahmoud Fawzy <mfhamy@wisys.com.sa>
2023-10-15 15:51:52 +0000

    Sync Commit @10/15/2023, 3:51:52 PM

---
+++ app/RiskManagment.Risks/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("RiskManagment.Risks.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ app/services.cds
@@ -0,0 +1,2 @@
+
+using from './RiskManagment.Risks/annotations';
